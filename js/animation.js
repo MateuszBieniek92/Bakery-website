28de0e8ab91e309170521d6c1ca84e8c79e29ea6
MateuszBieniek92 <bieniek.mateusz92@gmail.com>
2017-06-25 23:47:51 +0200

    Section 5 RWD v1.

--- js/animation.js
+++ js/animation.js
@@ -1,16 +1,22 @@
 $(function () {
 
-//variables 
-const mobile = window.matchMedia("screen and (max-width: 750px)");
-const sectionTwoPanels = $('.col-6');
+    //variables 
+    const mobile = window.matchMedia("screen and (max-width: 750px)");
+    const sectionTwoPanels = $('.col-6');
+    const sectionFivePanelLeft = $('.col-3');
+    const sectionFivePanelRight = $('.col-9');
+
 
 
     sectionTwoPanels.removeClass('col-12').addClass('col-6');
+    sectionFivePanelLeft.removeClass('col-12').addClass('col-3');
+    sectionFivePanelRight.removeClass('col-12').addClass('col-9');
     console.log(sectionTwoPanels);
     // mobile menu
     if (mobile.matches) {
         sectionTwoPanels.removeClass('col-6').addClass('col-12');
-        console.log('te');
+        sectionFivePanelLeft.removeClass('col-3').addClass('col-12');
+        sectionFivePanelRight.removeClass('col-9').addClass('col-12');
     }
 
 });
